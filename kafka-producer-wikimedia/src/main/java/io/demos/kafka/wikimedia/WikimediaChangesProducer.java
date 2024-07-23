451f211c68e5d42c0e22f855747b0c239a6035ad
TheLoneSheep <v.mxvlasov@gmail.com>
2024-07-24 02:11:02 +0300

    fix producer

--- kafka-producer-wikimedia/src/main/java/io/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -2,7 +2,10 @@ package io.demos.kafka.wikimedia;
 
 import com.launchdarkly.eventsource.EventSource;
 import com.launchdarkly.eventsource.EventHandler;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 
@@ -12,15 +15,20 @@ import java.util.concurrent.TimeUnit;
 
 public class WikimediaChangesProducer {
     public static void main(String[] args) throws InterruptedException {
-        String bootstrapServers = "127.0.0.1:9092";
+        //String bootstrapServers = "127.0.0.1:9092";
 
         // Create Producer Properties
         Properties properties = new Properties();
 
         // set Producer properties
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // set sage producer configs (Kafka <= 2.8)
+//        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+//        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+//        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
 
         // Create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
